742b67e63102498f88046b64b1d98285f486bba4
Rimjhim-tech <covidrimjhim@gmail.com>
2024-07-25 11:28:50 +0530

    update

---
+++ TimecompexitywithDebugging.java
@@ -0,0 +1,10 @@
+package DSJAVATRAINING;
+
+public class TimecompexitywithDebugging {
+    public static void main(String[] args) {
+        int i,n=8;
+        for (i=1;i<=n;i=i*2){
+            System.out.println("hello word");
+        }
+    }
+}
